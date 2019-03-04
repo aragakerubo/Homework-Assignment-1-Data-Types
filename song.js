8f1913d98ce7084b88940df7700c4a190be5d9c6
ogambakerubo <ogambahilda@gmail.com>
2019-03-04 21:04:36 +0300

    add song attributes and display them

---
+++ song.js
@@ -0,0 +1,33 @@
+//favourite song attributes
+var title = "Chandelier";
+var artist = "Sia";
+var album = "1000 Forms of Fear";
+var durationInSeconds = 216;
+var genre = "Electropop";
+var yearRecorded = 2013;
+var yearReleased = 2014;
+var label = {
+	record_label1: "Inertia",
+	record_label2: "Monkey Puzzle",
+	record_label3: "RCA",
+};
+var writers = {
+	song_writer1: "Sia Furler",
+	song_writer2: "Jesse Shatkin",
+};
+var producers = {
+	producer1: "Greg Kurstin",
+	producer2: "Jesse Shatkin",
+};
+
+//display song attributes
+console.log(title);
+console.log(artist);
+console.log(album);
+console.log(durationInSeconds);
+console.log(genre);
+console.log(yearReleased);
+console.log(label);
+console.log(writers);
+console.log(producers);
+
